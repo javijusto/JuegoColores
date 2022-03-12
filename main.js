490bd442ee5ab3f51e61010f31c9014589880dd3
JoanLara <joanpansu@gmail.com>
2022-03-12 12:06:39 +0100

    tercer commit

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-function numeroAleatorio(){
-    var aleatorios = [] 
-    for(let i=0; i<3; i++){
-        aleatorios[i] = Math.floor(Math.random()*256)
-    }
-    return aleatorios;
+function numeroAleatorio() {
+  var aleatorios = [];
+  for (let i = 0; i < 3; i++) {
+    aleatorios[i] = Math.floor(Math.random() * 256);
+  }
+  return aleatorios;
 }
 
-const cuadroIzquierda = document.querySelector('section p');
+const cuadroIzquierda = document.querySelector("section p");
 
 cuadroIzquierda.innerHTML = "<p>RGB: " + numeroAleatorio() + "</p>";
+
+const topBox = document.querySelector("#top");
+//prueba de vinculación
+topBox.setAttribute("style", "background-color: red");
