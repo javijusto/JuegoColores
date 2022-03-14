95f17a2354b917106d840e3ccbd196b6c85b94f0
JoanLara <joanpansu@gmail.com>
2022-03-14 14:27:55 +0100

    5o commit(función para vincular el color de manera distinta según rgb[0])

--- main.js
+++ main.js
@@ -15,8 +15,62 @@ console.log(rgb[0]);
 console.log(rgb.toString());
 
 const topBox = document.querySelector("#top");
+const midBox = document.querySelector("#mid");
+const botBox = document.querySelector("#bot");
 //prueba de vinculación
-topBox.setAttribute(
+/* topBox.setAttribute(
   "style",
   `background-color: rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`
-);
+  );
+  midBox.setAttribute(
+    "style",
+    `background-color: rgb(${rgb[0] + 30}, ${rgb[1]}, ${rgb[2] + 30})`
+    );
+    botBox.setAttribute(
+      "style",
+      `background-color: rgb(${rgb[0] + 40}, ${rgb[1] + 40}, ${rgb[2]})`
+      ); */
+colorAleatorio(rgb);
+function colorAleatorio(rgb) {
+  if (rgb[0] < 85) {
+    topBox.setAttribute(
+      "style",
+      `background-color: rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`
+    );
+    midBox.setAttribute(
+      "style",
+      `background-color: rgb(${rgb[0] + 30}, ${rgb[1]}, ${rgb[2] + 30})`
+    );
+    botBox.setAttribute(
+      "style",
+      `background-color: rgb(${rgb[0] + 40}, ${rgb[1] + 40}, ${rgb[2]})`
+    );
+  }
+  if (rgb[0] > 170) {
+    topBox.setAttribute(
+      "style",
+      `background-color: rgb(${rgb[0] + 30}, ${rgb[1]}, ${rgb[2] + 30})`
+    );
+    midBox.setAttribute(
+      "style",
+      `background-color: rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`
+    );
+    botBox.setAttribute(
+      "style",
+      `background-color: rgb(${rgb[0] + 40}, ${rgb[1] + 40}, ${rgb[2]})`
+    );
+  } else {
+    topBox.setAttribute(
+      "style",
+      `background-color: rgb(${rgb[0] + 30}, ${rgb[1]}, ${rgb[2] + 30})`
+    );
+    midBox.setAttribute(
+      "style",
+      `background-color: rgb(${rgb[0] + 40}, ${rgb[1] + 40}, ${rgb[2]})`
+    );
+    botBox.setAttribute(
+      "style",
+      `background-color: rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`
+    );
+  }
+}
